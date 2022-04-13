a35f47cdd544b31b60b99e3c694e624d836cb28e
MasterTemple <dgmastertemple@protonmail.com>
2022-04-13 16:26:57 -0700

    added some keyboard shortcuts: new note, cancel note, switch tabs

---
+++ WebHelpDesk/index.js
@@ -0,0 +1,47 @@
+/*
+Web Help Desk Keyboard Short Cuts
+*/
+
+document.addEventListener("keydown", function (event) {
+  // ALT + N create new tech note
+  if (event.altKey && event.key === "n") {
+    try {
+      let cancelButton = document.querySelector("#TechNoteEditorUpdateContainer > table > tbody > tr:nth-child(6) > td > div > a:nth-child(1)");
+      // if note is already open
+      if(cancelButton) {
+        // close note
+        cancelButton.click();
+      }
+      // create new note
+      else {
+        document.querySelector("#NoteListUpdateDiv > table > tbody > tr.header > td.newNoteHeader > a").click();
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  // ALT + A goes to asset tab
+  else if(event.altKey && event.key === "a") {
+    try {
+      document.querySelector("#TabPanelUpdateContainer > table > tbody > tr:nth-child(1) > td:nth-child(1) > table > tbody > tr > td:nth-child(2) > div").click();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  // ALT + C goes to client tab
+  else if(event.altKey && event.key === "c") {
+    try {
+      document.querySelector("#TabPanelUpdateContainer > table > tbody > tr:nth-child(1) > td:nth-child(1) > table > tbody > tr > td:nth-child(1) > div").click();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  // ALT + T goes to ticket tab
+  else if(event.altKey && event.key === "t") {
+    try {
+      document.querySelector("#TabPanelUpdateContainer > table > tbody > tr:nth-child(1) > td:nth-child(1) > table > tbody > tr > td:nth-child(3) > div").click();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+});
