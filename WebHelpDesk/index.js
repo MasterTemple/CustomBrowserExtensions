539442ac61b88e1f1af028e83741dc9f9b4d913b
MasterTemple <54373793+MasterTemple@users.noreply.github.com>
2022-05-18 11:30:29 -0700

    auto format emails + formstack

--- WebHelpDesk/index.js
+++ WebHelpDesk/index.js
@@ -2,6 +2,7 @@
 Web Help Desk Keyboard Short Cuts
 RegEx to match keybinds: /(?<=\/\/ )(ALT|ENTER)[^\n]+/gim
 */
+
 var index = 3;
 
 document.addEventListener("keydown", function (event) {
@@ -56,6 +57,28 @@ document.addEventListener("keydown", function (event) {
           /(?<=[^\]])a\d{6}/gim,
           (m) => `[b]${m.toUpperCase()}[/b]`
         );
+        // bolds urls but unnecessary
+        // element.value = element.value.replace(
+        //   /(?<=[^\]][^ ])https?:\/\/\S+/gim,
+        //   (m) => `[b] ${m} [/b]`
+        // );
+        // // quote an email
+        // if (element.value.match(/^From:/g))
+        //   element.value = `[quote]${element.value}[/quote]`;
+        // // bold subject line
+        // if (element.value.match(/(?<=^Subject:).*/gim))
+        //   element.value = element.value.replace(
+        //     /(?<=^Subject:).*/gim,
+        //     (m) => `[b]${m}[/b]`
+        //   );
+
+        // mark visible to client if email
+        if (element.value.match(/^(\[quote\])?From:/g)) {
+          document.querySelector(
+            "#TechNoteEditorUpdateContainer > table > tbody > tr:nth-child(1) > td.dataTop > table > tbody > tr:nth-child(2) > td:nth-child(1) > table > tbody > tr > td:nth-child(1) > input[type=checkbox]"
+          ).checked = true;
+        }
+        // find save button
         let buttonSection = [
           ...document.querySelectorAll(
             "#TechNoteEditorUpdateContainer > table > tbody > tr"
@@ -156,5 +179,55 @@ document.addEventListener("keydown", function (event) {
     } catch (error) {
       console.log(error);
     }
+  }
+  // ALT + R expands request type select menu
+  else if (event.altKey && event.key === "r") {
+    try {
+      ExpandSelect(
+        [...document.getElementById("ticketRequestTypeObserverDiv").childNodes]
+          .filter((t) => t.tagName === "SELECT")
+          .at(-1)
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }
+});
+
+// format on copy/paste
+document.addEventListener("paste", async (event) => {
+  let element = event.target;
+  let text = await navigator.clipboard.readText();
+  text = text.replace(/\r/gim, "");
+  text = text.replace(/^\n+---------- Forwarded message ---------\n/g, "");
+  let isEmail = text.match(/^From:/g);
+  if (isEmail) {
+    let isFormstack = text.match(
+      /^From: OnceHub Mailer \<mailer@oncehub.com\>/g
+    );
+    // is formstack form
+    if (isFormstack) {
+      // bolds next line after the following headers
+      // first group is `text + newline` (for field being on next line)
+      // second group is `text:` (for field being on same line)
+      text = text.replace(
+        /(?<=((To: <it.helpdesk@biola.edu>\n\n\n |Booking page image|Subject|Technical Support Appointment|Calendar|Your time|Customer time|Location|Booking ID|Customer name|Phone number|Customer's mobile phone|Type of Assistance|What type of computer is being updated\?|Best number to reach you|Asset Number|Computer OS Update Time Slot|Ticket Number)\n)|((Passcode:|Meeting ID:|Meeting passcode:) ?)).*/gm,
+        (m) => `[b]${m}[/b]`
+      );
+    }
+    // bold sender
+    text = text.replace(/(?<=^From: )[^\<]+/gim, (m) => `[b]${m}[/b]`);
+    // bold subject line
+    text = text.replace(/(?<=^Subject: ).*/gim, (m) => `[b]${m}[/b]`);
+    // removes ticket thread
+    text = text.replace(
+      /On .* Biola IT Helpdesk <it.helpdesk@biola.edu> wrote:[^]+/gim,
+      ""
+    );
+
+    // quotes email
+    text = `[quote]${text}[/quote]`;
+    element.value = text;
+    event.preventDefault();
   }
 });
